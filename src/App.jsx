0004c584837ab302a19090e09565f523665ad26e
josephrubiojimenez <josephjimeneeez@gmail.com>
2026-08-14 17:39:26 +0800

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="bg-[#fffbf1] text-[100px] text-[#000000] min-h-dvh border border-[#000000] flex items-center justify-center">
-      Hello World
+      Hello World!
     </div>
   );
 }
