69e68cc890874a2299d04a0fb3335febe296d327
josephrubiojimenez <josephjimeneeez@gmail.com>
2026-08-14 17:25:25 +0800

    center div dawg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="bg-[#fffbf1] text-[100px] text-[#000000] text-center min-h-dvh border border-[#000000] justify-center margin-top-[20px]">
+    <div className="bg-[#fffbf1] text-[100px] text-[#000000] min-h-dvh border border-[#000000] flex items-center justify-center">
       Hello World
     </div>
   );
