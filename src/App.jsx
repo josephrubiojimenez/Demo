65114226900222ae851a1582d543377f01b84400
josephrubiojimenez <josephjimeneeez@gmail.com>
2026-08-14 17:19:40 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-[#fffbf1] text-[100px] text-[#000000] text-center min-h-dvh border border-[#000000] justify-center margin-top-[20px]">
+      Hello World
+    </div>
+  );
+}
+export default App;
+ 
